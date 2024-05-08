9ca5c16005536a91006fc32137e6d4682e9d00e0
Isabelle Costa <isabelle.costa@wexinc.com>
2024-05-08 14:12:42 -0300

    integrating databse postgresql

--- src/main/application/main.go
+++ src/main/application/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type UserData struct {
@@ -16,13 +18,24 @@ type UserData struct {
 	Age       int    `json:"age"`
 }
 
-var users = map[string]UserData{}
-var currentID int
+var db *sql.DB
+// var users = map[string]UserData{}
+// var currentID int
 
+func initDB(){
+	var err error
+	db, err = sql.Open("postgres", "postgres://user:password@localhost/database_name?sslmode=disable")
+	if err !nil {
+		panic(err)
+	}
+}
+
+/*
 func generateID() string {
 	currentID++
 	return strconv.Itoa(currentID)
 }
+*/
 
 func saveUser(c *gin.Context) {
 	var newUser UserData
@@ -52,6 +65,35 @@ func saveUser(c *gin.Context) {
 		return
 	}
 
+	var userID string
+
+	if newUser.ID != "" {
+		err := db.QueryRow("SELECT id FROM users WHERE id=$1", newUser.ID).Scan(&userID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User Not Found"})
+			return
+		}
+		_, err = db.Exec("UPDATE users SET first_name=$1, last_name=$2, email=$3, age=$4 WHERE id=$5", newUser.FirstName, newUser.LastName, newUser.Email, newUser.Age, newUser.ID)
+		
+		if err !nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
+			return
+		}
+	} else {
+		userID = generateID()
+		_, err := db.Exec("INSERT INTO users (id, first_name, last_name, email, age) VALUES ($1, $2, $3, $4, $5)", userID, newUser.FirstName, newUser.LastName, newUser.Email, newUser.Age)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save user"})
+			return
+		}
+	}
+	
+		newUser.ID = userID
+		c.JSON(http.StatusOK, gin.H{"message": "User updated successfully", "updated_user": newUser})
+
+	}
+
+	/*
 	if newUser.ID != "" {
 		_, exists := users[newUser.ID]
 		if !exists {
@@ -66,9 +108,19 @@ func saveUser(c *gin.Context) {
 	newUser.ID = generateID()
 	users[newUser.ID] = newUser
 	c.JSON(http.StatusCreated, newUser)
+	*/
 }
 
 func findUser(c *gin.Context) {
+	userID := c.Param("id")
+	var user UserData
+	err := db.QueryRow("SELECT id, first_name, last_name, email, age FROM users WHERE ID=$1", userID).Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Age)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User Not Found"})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+	/*
 	userID := c.Param("id")
 
 	user, exists := users[userID]
@@ -78,9 +130,13 @@ func findUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, user)
+	*/
 }
 
 func main() {
+
+	initDB()
+
 	router := gin.Default()
 	router.POST("/save", saveUser)
 	router.GET("/find/:id", findUser)
