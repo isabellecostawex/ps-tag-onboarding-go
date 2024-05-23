95802f9bcea1ba3ce31f81a9af71e03cb306651f
Isabelle Costa <isabelle.costa@wexinc.com>
2024-05-23 09:35:05 -0300

    new service to retrieve an user

---
+++ internal/services/retrieve_user_service.go
@@ -0,0 +1,21 @@
+package services
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/isabellecostawex/ps-tag-onboarding-go/internal/models"
+	"github.com/isabellecostawex/ps-tag-onboarding-go/pkg/postgresql"
+)
+
+func RetrieveUser(userID string)(models.UserData, error) {
+	var user models.UserData
+	err := postgresql.db.QueryRow("SELECT id, first_name, last_name, email, age FROM users WHERE ID=$1", userID).Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Age)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return user, errors.New("User not found")
+		}
+		return user, err
+	}
+	return user, nil
+}
