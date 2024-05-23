8978dd2bff3504baa8d002459c9a34d382596250
Isabelle Costa <isabelle.costa@wexinc.com>
2024-05-23 09:34:39 -0300

    new file with the struct

---
+++ internal/models/user.go
@@ -0,0 +1,9 @@
+package models
+
+type UserData struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Age       int    `json:"age"`
+}
