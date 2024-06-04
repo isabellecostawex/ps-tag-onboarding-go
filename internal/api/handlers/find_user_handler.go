78879a0e0218098ca714e7a8d0e765aa2f64c156
Isabelle Costa <isabelle.costa@wexinc.com>
2024-06-04 08:09:37 -0300

    changing handlers for them stop interacting with the database

--- internal/api/handlers/find_user_handler.go
+++ internal/api/handlers/find_user_handler.go
@@ -4,15 +4,20 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-
+	"github.com/isabellecostawex/ps-tag-onboarding-go/internal/domain/users/postgres"
 	"github.com/isabellecostawex/ps-tag-onboarding-go/internal/services"
 )
 
-func FindUserHandler(c *gin.Context){
+func FindUserHandler(c *gin.Context) {
 	userID := c.Param("id")
-	user, err := services.RetrieveUser(userID)
+
+	userRepo := &postgres.UsersRepository{}
+	usersService := services.UserManagementService{UserRepo: userRepo}
+
+	user, err := usersService.RetrieveUser(userID)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{ "error": "User not found"})
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
 	}
 	c.JSON(http.StatusOK, user)
 }
--- internal/api/handlers/save_user_handler.go
+++ internal/api/handlers/save_user_handler.go
@@ -2,58 +2,30 @@ package handlers
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/isabellecostawex/ps-tag-onboarding-go/internal/models"
+	"github.com/isabellecostawex/ps-tag-onboarding-go/internal/domain/users"
+	"github.com/isabellecostawex/ps-tag-onboarding-go/internal/domain/users/postgres"
 	"github.com/isabellecostawex/ps-tag-onboarding-go/internal/services"
 )
 
-func SaveUserHandler (c *gin.Context) {
-	var newUser models.UserData
+func SaveUserHandler(c *gin.Context) {
+    var newUser users.UserData
 
-	if err := c.ShouldBindJSON(&newUser); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data in request body"})
-		return
-	}
-	if newUser.Age < 18 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "User did not pass validation", "details": "User does not meet minimum age requirement"})
-		return
-	}
-		
-	if newUser.FirstName == "" || newUser.LastName == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "User did not pass validation", "details": "User first/last names required"})
-		return
-	}
-		
-	if newUser.Email == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "User did not pass validation", "details": "User email required"})
-		return
-	}
-		
-	if !strings.Contains(newUser.Email, "@") {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "User did not pass validation", "details": "User email must be properly formatted"})
-		return
-	}
+    if err := c.ShouldBindJSON(&newUser); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data in request body"})
+        return
+    }
 
-	var userID int
-	var err error
+    userRepo := &postgres.UsersRepository{}
+    usersService := services.UserManagementService{UserRepo: userRepo}
 
-	if newUser.ID != 0 {
-		err = services.UpdateUser(&newUser)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
-			return
-		}
-		userID = newUser.ID
-	} else {
-		userID, err = services.CreateUser(&newUser)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save user"})
-			return
-		}
-	}
-	newUser.ID = userID
-	c.JSON(http.StatusOK, gin.H{"message": "User saved successfully", "user": newUser})
+    userID, err := usersService.SaveUser(newUser)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    newUser.ID = userID
+    c.JSON(http.StatusOK, gin.H{"message": "User saved successfully", "user": newUser})
 }
