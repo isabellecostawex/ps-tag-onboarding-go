bf02dd54367b4b976e8dff322811ab9af8f1814f
Isabelle Costa <isabelle.costa@wexinc.com>
2024-06-27 14:05:47 -0300

    fixing handlers

--- internal/api/handlers/v1/routes.go
+++ internal/api/handlers/v1/routes.go
@@ -1,8 +1,9 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(router *gin.Engine) {
-	router.POST("/save", SaveUserHandler)
-	router.GET("find/:id", FindUserHandler)
+	RegisterRoutes(router)
 }
