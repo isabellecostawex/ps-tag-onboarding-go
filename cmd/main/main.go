3b23915f9b5ac86a3a8578317b8cf542783d801a
Isabelle Costa <isabelle.costa@wexinc.com>
2024-08-12 10:00:49 -0300

    reemoving pakage log from main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-	
 	"github.com/gin-gonic/gin"
 	"github.com/isabellecostawex/ps-tag-onboarding-go/pkg/postgresql"
 	"github.com/isabellecostawex/ps-tag-onboarding-go/internal/api/handlers"
